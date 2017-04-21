8312edf14b3cf8077aa82c0ff6d6fe0628191172
Tony Theodoropoulos <theodot@outlook.com>
2017-04-21 02:25:12 -0400

    first commit

---
+++ TeamProjectTest/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TeamProjectTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.ClickButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(166, 237);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(51, 20);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "label1";
+            // 
+            // ClickButton
+            // 
+            this.ClickButton.Location = new System.Drawing.Point(170, 137);
+            this.ClickButton.Name = "ClickButton";
+            this.ClickButton.Size = new System.Drawing.Size(75, 36);
+            this.ClickButton.TabIndex = 1;
+            this.ClickButton.Text = "Click Me";
+            this.ClickButton.UseVisualStyleBackColor = true;
+            this.ClickButton.Click += new System.EventHandler(this.ClickButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 423);
+            this.Controls.Add(this.ClickButton);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button ClickButton;
+    }
+}
+
